aefd65f4b5da0d24d606716d1dc4bbf5bb8c0af6
friedricepro <yoshbosh099@gmail.com>
2025-09-18 19:38:59 -0700

    Create GroundPainter.java

---
+++ GroundPainter.java
@@ -0,0 +1,33 @@
+import org.code.neighborhood.*;
+public class GroundPainter extends BackgroundPainter{
+
+
+  /* ------------------ paintGrass ------------------
+   * paintGrass is a method used to paint 2 rows of 
+   green at the bottom for the building that 
+   BuildingPainter will create on top of, uses paintRow
+   from BackgroundPainter to simplify the code
+   * --------------------------------------------------
+   */
+  
+  public void paintGrass(){
+while(canMove("north")){
+
+  paintRow("Green");
+
+  if(!canMove("west")){
+    turnRight();
+    move();
+    turnRight();
+  }
+  //ends the while loop when the painter faces east after finishing the 2nd row
+  if(!canMove("east")) {
+  break;
+  }
+}
+
+
+    
+  }
+}
+  
